cf43e21210a1ff5b2109aab21bb131ec42b29dc0
Zeef0 <josealfonsor357@gmail.com>
2022-12-26 16:17:07 +0800

    added reject and approve action in admin for appointment administration

--- booking/appointment/admin.py
+++ booking/appointment/admin.py
@@ -2,3 +2,17 @@ from django.contrib import admin
 from .models import Appointments
 
 # Register your models here.
+
+@admin.action(description='Approve Appointments')
+def approve_appointment(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+@admin.action(description='Reject Appointments')
+def reject_appointment(modeladmin, request, queryset):
+    queryset.update(is_approved=False)
+
+@admin.register(Appointments)
+class AppoinmentsAdmin(admin.ModelAdmin):
+    list_display = ['username', 'service', 'date', 'appointment_time', 'is_approved']
+    ordering = ['-date', 'appointment_time']
+    actions = [approve_appointment, reject_appointment]
