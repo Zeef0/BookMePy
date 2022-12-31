08b8b3a7831971694e91d7ca45d1a2535c50b96f
Zeef0 <josealfonsor357@gmail.com>
2022-12-31 16:03:15 +0800

    Created a cbv  for deleting and updating an appointment

--- booking/appointment/urls.py
+++ booking/appointment/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('appointments/', views.AppointmentsListView.as_view(), name='all_appointments'),
     path('user/panel', views.UserPanel.as_view(), name='user_panel'),
     path('appointment/<int:pk>', views.AppointmentDetailView.as_view(), name="appointment_detail"),
-    path('book/appointment/', views.CreateAppointmentView.as_view(), name="book_appointment")
+    path('book/appointment/', views.CreateAppointmentView.as_view(), name="book_appointment"),
+    path('edit/appointment/<int:pk>', views.UpdateAppointmentView.as_view(), name='update_appointment'),
+    path('delete/appointment/<int:pk>', views.DeleteAppointmentView.as_view(), name="delete_appointment")
 ]
--- booking/appointment/views.py
+++ booking/appointment/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Appointments
 from django.views.generic import ListView, CreateView, DetailView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import UpdateView, DeleteView
 
 from .forms import AppointmentForm
 
@@ -17,6 +19,7 @@ class AppointmentDetailView(LoginRequiredMixin, DetailView):
 class UserPanel(LoginRequiredMixin, ListView):
     model = Appointments
     context_object_name = "context"
+
 
     def get_queryset(self):
         qs = Appointments.objects.all().filter(username=self.request.user)
@@ -38,5 +41,25 @@ class CreateAppointmentView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class UpdateAppointmentView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Appointments
+    form_class = AppointmentForm
+
+    def test_func(self):
+        obj = self.get_object()
+        # Test current user should be equal to the owner of the appointment
+        return self.request.user == obj.username
+
+    def form_valid(self, form):
+        form.instance.username = self.request.user
+        return super().form_valid(form)
 
 
+class DeleteAppointmentView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Appointments
+    success_url = reverse_lazy('user_panel')
+    context_object_name = 'appointment'
+    def test_func(self):
+        obj = self.get_object()
+        # Test current user should be equal to the owner of the appointment
+        return self.request.user == obj.username or self.request.user.is_staff()
